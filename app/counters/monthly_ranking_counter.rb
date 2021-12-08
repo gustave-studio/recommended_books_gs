145f1ba8f0634429bed73773624421c8a4d35f9c
gustave-studio <gustave0222@gmail.com>
2021-12-08 22:59:31 +0900

    🚧リンクの数といいねの数で集計

--- app/counters/monthly_ranking_counter.rb
+++ app/counters/monthly_ranking_counter.rb
@@ -2,11 +2,26 @@ class MonthlyRankingCounter
   class << self
     def count
       period = Range.new(1.month.ago.beginning_of_day, Time.zone.now.beginning_of_day)
-      book_data = RecommendedBook.where(article_created_at: period).group("asin").count
+      recommended_data = RecommendedBook.where(article_created_at: period).group(:isbn, :user_id, :likes_count).select(:isbn, :likes_count).map{ |data| { isbn: data.isbn, likes_count: data.likes_count } }
+
+      book_data_count = recommended_data.flatten.each_with_object([]) { |value, result|
+        a = result.find do |result|
+          result[:isbn].include?(value[:isbn])
+        end
+
+        if a
+          a[:likes_count] += value[:likes_count]
+          a[:count] += 1
+        else
+          result << { isbn: value[:isbn], likes_count: value[:likes_count], count: 1 }
+        end
+      }
+
+      sorted_book_data = book_data_count.sort_by { |h| h.values_at(:count, :likes_count) }.reverse
 
       ActiveRecord::Base.transaction do
-        book_data.each do |data|
-          MonthlyRanking.create(asin: data[0], count: data[1])
+        sorted_book_data.each.with_index(1) do |data, index|
+          MonthlyRanking.create(isbn: data[:isbn], count: data[:count], likes_count: data[:likes_count], ranking: index)
         end
       end
     end
