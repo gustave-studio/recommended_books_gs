5e5e0ed534decd143d39facbae651fd8f1cb53d5
gustave-studio <gustave0222@gmail.com>
2022-01-02 04:45:32 +0900

    ✨6ヶ月ランキングをキャッシュする

--- app/controllers/api/six_months_rankings_controller.rb
+++ app/controllers/api/six_months_rankings_controller.rb
@@ -1,5 +1,11 @@
 class Api::SixMonthsRankingsController < ApplicationController
   def index
-    @six_months_rankings = SixMonthsRanking.order('ranking ASC').limit(10)
+    @six_months_rankings ||= cache_ranking
+  end
+
+  private def cache_ranking
+    Rails.cache.fetch("six_months_ranking", expires_in: 24.hours) do
+      SixMonthsRanking.order('ranking ASC').limit(10)
+    end
   end
 end
