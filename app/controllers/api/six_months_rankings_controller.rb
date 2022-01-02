2d60406a172b82d753269fd37827f160c8641303
gustave-studio <gustave0222@gmail.com>
2022-01-02 10:59:09 +0900

    ⏪️実装を戻して様子を見る

--- app/controllers/api/six_months_rankings_controller.rb
+++ app/controllers/api/six_months_rankings_controller.rb
@@ -1,11 +1,5 @@
 class Api::SixMonthsRankingsController < ApplicationController
   def index
-    @six_months_rankings ||= cache_ranking
-  end
-
-  private def cache_ranking
-    Rails.cache.fetch("six_months_ranking", expires_in: 24.hours) do
-      SixMonthsRanking.order('ranking ASC').limit(10)
-    end
+    @six_months_rankings = SixMonthsRanking.order('ranking ASC').limit(10)
   end
 end
