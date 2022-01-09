acc27aa0d577d890860bc9f732138bab028b9202
gustave-studio <gustave0222@gmail.com>
2022-01-10 07:09:34 +0900

    ✨categoryを指定した場合はcategoryで絞り込み検索

--- app/controllers/api/editorial_department_recommendations_controller.rb
+++ app/controllers/api/editorial_department_recommendations_controller.rb
@@ -1,5 +1,11 @@
 class Api::EditorialDepartmentRecommendationsController < ApplicationController
   def index
-    @recommendations = EditorialDepartmentRecommendation.order(order: 'ASC')
+    category = params[:category]
+
+    if category
+      @recommendations = EditorialDepartmentRecommendation.where(category: category).order(order: 'ASC')
+    else
+      @recommendations = EditorialDepartmentRecommendation.order(order: 'ASC')
+    end
   end
 end
